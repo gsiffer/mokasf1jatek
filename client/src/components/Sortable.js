0dffa66bb14f8712315c6507d3c4840d29a52f3d
Gabor Siffer <gabor.siffer@gmail.com>
2024-07-16 10:12:40 -0700

    update

--- client/src/components/Sortable.js
+++ client/src/components/Sortable.js
@@ -1,20 +1,20 @@
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
+// import { SortableContainer, SortableElement } from "react-sortable-hoc";
 
-// SortableItem component
-const SortableItem = SortableElement(({ value }) => <li>{value.content}</li>);
+// // SortableItem component
+// const SortableItem = SortableElement(({ value }) => <li>{value.content}</li>);
 
-// SortableList component
-const SortableList = SortableContainer(({ items }) => (
-  <ul>
-    {items.map((value, index) => (
-      <SortableItem key={`item-${value.id}`} index={index} value={value} />
-    ))}
-  </ul>
-));
+// // SortableList component
+// const SortableList = SortableContainer(({ items }) => (
+//   <ul>
+//     {items.map((value, index) => (
+//       <SortableItem key={`item-${value.id}`} index={index} value={value} />
+//     ))}
+//   </ul>
+// ));
 
-// SortableComponent combines SortableList and SortableItem
-const Sortable = ({ items, onSortEnd }) => (
-  <SortableList items={items} onSortEnd={onSortEnd} />
-);
+// // SortableComponent combines SortableList and SortableItem
+// const Sortable = ({ items, onSortEnd }) => (
+//   <SortableList items={items} onSortEnd={onSortEnd} />
+// );
 
-export default Sortable;
+// export default Sortable;
--- client/src/pages/dashboard/TeamStandings.js
+++ client/src/pages/dashboard/TeamStandings.js
@@ -351,12 +351,12 @@
 
 import React, { useState } from "react";
 import styled from "styled-components";
-import {
-  SortableContainer,
-  SortableElement,
-  arrayMove,
-} from "react-sortable-hoc";
-import Sortable from "../../components/Sortable";
+// import {
+//   SortableContainer,
+//   SortableElement,
+//   arrayMove,
+// } from "react-sortable-hoc";
+// import Sortable from "../../components/Sortable";
 
 const Wrapper = styled.section`
   .team-container ul {
@@ -395,34 +395,34 @@ const Wrapper = styled.section`
 //   );
 // });
 
+//************************************************************************************************************************
+
 const TeamStandings = () => {
-  const [items, setItems] = useState([
-    { id: "1", content: "Item 1" },
-    { id: "2", content: "Item 2" },
-    { id: "3", content: "Item 3" },
-    { id: "4", content: "Item 4" },
-    { id: "5", content: "Item 5" },
-    { id: "6", content: "Item 6" },
-    { id: "7", content: "Item 7" },
-    { id: "8", content: "Item 8" },
-    { id: "9", content: "Item 9" },
-    { id: "10", content: "Item 10" },
-    { id: "11", content: "Item 11" },
-    { id: "12", content: "Item 12" },
-  ]);
-
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    setItems(arrayMove(items, oldIndex, newIndex));
-  };
-
-  return (
-    <Wrapper>
-      <div className="team-container">
-        <Sortable items={items} onSortEnd={onSortEnd} />
-        <div>{JSON.stringify(items, null, 2)}</div>
-      </div>
-    </Wrapper>
-  );
+  // const [items, setItems] = useState([
+  //   { id: "1", content: "Item 1" },
+  //   { id: "2", content: "Item 2" },
+  //   { id: "3", content: "Item 3" },
+  //   { id: "4", content: "Item 4" },
+  //   { id: "5", content: "Item 5" },
+  //   { id: "6", content: "Item 6" },
+  //   { id: "7", content: "Item 7" },
+  //   { id: "8", content: "Item 8" },
+  //   { id: "9", content: "Item 9" },
+  //   { id: "10", content: "Item 10" },
+  //   { id: "11", content: "Item 11" },
+  //   { id: "12", content: "Item 12" },
+  // ]);
+  // const onSortEnd = ({ oldIndex, newIndex }) => {
+  //   setItems(arrayMove(items, oldIndex, newIndex));
+  // };
+  // return (
+  //   <Wrapper>
+  //     <div className="team-container">
+  //       <Sortable items={items} onSortEnd={onSortEnd} />
+  //       <div>{JSON.stringify(items, null, 2)}</div>
+  //     </div>
+  //   </Wrapper>
+  // );
 };
 
 export default TeamStandings;

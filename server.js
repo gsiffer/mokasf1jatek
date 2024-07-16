a7856b7bb949818565bc1a7e25c83a228d35aa9b
Gabor Siffer <gabor.siffer@gmail.com>
2024-07-16 13:17:33 -0700

    updates

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ if (process.env.NODE_ENV !== "production") {
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Enable trust proxy
+app.set("trust proxy", 1);
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
 app.use(cookieParser());
